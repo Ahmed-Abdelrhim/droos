50f483dde256c1ba695d5806de51984af5cf7a9b
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-09-11 04:13:57 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Student\Auth\CustomLoginController;
 /*
 |--------------------------------------------------------------------------
 | Student Routes
@@ -12,11 +14,12 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
