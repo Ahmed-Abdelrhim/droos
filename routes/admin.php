b707e95eb36ef9bcc3522584af4d98830ff30c4b
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-09-22 16:34:02 +0200

    delete course

--- routes/admin.php
+++ routes/admin.php
@@ -71,7 +71,12 @@ Route::group(['middleware' => 'disable_back_btn'], function () {
         Route::get('edit/courses/3rd/year/{id}', [AcademicThirdYear::class, 'showCourseEditForm']);
         Route::post('edit/courses/3rd/year/{id}', [AcademicThirdYear::class, 'updateCourse'])->name('edit.course.3rd');
         Route::post('delete/courses/3rd/year/{id}', [AcademicThirdYear::class, 'deleteCourse'])->name('delete.course.3rd');
+
+        //Admin Profile
         Route::get('admin/profile',[DashboardController::class,'showTeacherProfile'])->name('teacher.profile');
+
+
+
 
     });
 
