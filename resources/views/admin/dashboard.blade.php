5bec81ad2de4b69d9cecac7636e10d74f8f8909f
Anas Rabea <anas.rabea100@gmail.com>
2022-09-24 23:20:08 +0200

    admin dashbord design

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,35 +1,37 @@
 @extends('layouts.design')
 @section('content')
+
 <style>
 
 body{
    background-color: var(--light-bg);
-   padding-left: 30rem;
+   padding-right: 30rem;
 }
 
 body.active{
-   padding-left: 0;
+   padding-right: 0;
 }
 
 
 .side-bar{
    position: fixed;
-   top: 0; left: 0;
+   top: 0; right: 0;
    width: 30rem;
    background-color: var(--white);
    height: 100vh;
    border-right: var(--border);
    z-index: 1200;
+   transition: .2s linear;
 }
 
 .side-bar #close-btn{
-   text-align: right;
+   text-align: left;
    padding: 2rem;
    display: none;
 }
 
 .side-bar #close-btn i{
-   text-align: right;
+   text-align: left;
    font-size: 2.5rem;
    background:var(--red);
    border-radius: .5rem;
@@ -78,7 +80,7 @@ body.active{
 }
 
 .side-bar .navbar a i{
-   margin-right: 1.5rem;
+   margin-left: 1.5rem;
    color:var(--main-color);
    transition: .2s linear;
 }
@@ -96,20 +98,162 @@ body.active{
 }
 
 .side-bar.active{
-   left: -30rem;
+   right: -30rem;
 }
 
+
+.side-bar #close-btn{
+   text-align: left;
+   padding: 2rem;
+   display: none;
+}
+
+.side-bar #close-btn i{
+   text-align: left;
+   font-size: 2.5rem;
+   background:var(--red);
+   border-radius: .5rem;
+   color:var(--white);
+   cursor: pointer;
+   height: 4.5rem;
+   width: 4.5rem;
+   line-height: 4.5rem;
+   text-align: center;
+}
+
+.side-bar #close-btn i:hover{
+   background-color: var(--black);
+}
+
+
+.tutor {
+    font-size: 1.8rem;
+    color: var(--light-color);
+    display:flex;
+}
+
+.tutor img {
+    height: 5rem;
+    width: 5rem;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.tutor h3 {
+    font-size: 1.8rem;
+    color: var(--black);
+    margin-bottom: .2rem;
+}
+
+.tutor span {
+    font-size: 1.3rem;
+    color: var(--light-color);
+}
+
+.logo img {
+    height: 8rem;
+    width: 15rem;
+}
+
+.thumb #video {
+    width: 100%;
+    height:500px;
+    border-radius: 5px;
+}
+
+@media (max-width: 767px){
+    .thumb #video {
+        width: 100%;
+    }
+}
+
+@media (max-width:1200px){
+
+   body{
+      padding-right: 0;
+   }
+
+   .side-bar{
+      right: -30rem;
+      transition: .2s linear;
+   }
+
+   .side-bar #close-btn{
+      display: block;
+   }
+
+   .side-bar.active{
+      right: 0;
+      box-shadow: 0 0 0 100vw rgba(0,0,0,.8);
+      border-right: 0;
+   }
+
+}
+
+
+/* Style the sidenav links and the dropdown button */
+.dropdown-btn {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 24px;
+  color: #818181;
+  display: block;
+  border: none;
+  background: none;
+  width:100%;
+  text-align: right;
+  cursor: pointer;
+  outline: none;
+}
+
+/* On mouse-over */
+.dropdown-btn:hover {
+  color: #f1f1f1;
+}
+
+
+/* Add an active class to the active dropdown button */
+.active {
+  background-color: var(--main-color);
+  color: white;
+}
+
+/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+.dropdown-container {
+  display: none;
+  background-color: #000;
+  padding-right: 8px;
+}
+
+/* Optional: Style the caret down icon */
+.fa-caret-down {
+  float: left;
+  padding-left: 8px;
+}
 </style>
 <header class="header">
-
-   <section class="flex">
-      <div class="icons">
-         <div id="menu-btn" class="fas fa-bars"></div>
-      </div>
-   </section>
-
+    <!-- <img id="back-ground" src="{{asset('images/back-ground.png')}}"> -->
+    <section class="flex">
+            <div class="icons">
+                <div id="menu-btn" class="fas fa-bars"></div>
+                <div id="toggle-btn" class="fas fa-sun"></div>
+                <div id="user-btn" class="fas fa-user"></div>
+            </div>
+            <div class="profile">
+                <img src="{{asset('images/pic-1.jpg')}}" class="image" alt="">
+                <h3 class="name">{{Auth::guard('admin')->user()->name}}</h3>
+                <p class="role">Admin</p>
+                <a href="{{route('teacher.profile')}}" class="btn">view profile</a>
+            </div>
+            <div class="tutor">
+                <div class="info">
+                    <span>Welcome</span>
+                    <h3>{{Auth::guard('admin')->user()->name}}</h3>
+                </div>
+                <img src="{{asset('images//pic-6.jpg')}}" alt="tutor">
+            </div>
+    </section>
 </header>
-
 
 <div class="side-bar">
 
@@ -117,152 +261,251 @@ body.active{
       <i class="fas fa-times"></i>
    </div>
 
-   <div class="profile">
-      <img src="{{asset('images/pic-1.jpg')}}" class="image" alt="">
-      <h3 class="name">{{Auth::guard('admin')->user()->name}}</h3>
-      <p class="role">Admin</p>
-      <a href="{{route('teacher.profile')}}" class="btn">view profile</a>
+   <div class="logo">
+      <a href="{{asset('home')}}" class="logo"><img src="{{asset('images/msbah.png')}}"></a>
    </div>
 
-   <nav class="navbar">
-{{--      <a href="home.html"><i class="fas fa-home"></i><span>home</span></a>--}}
+    <nav class="navbar">
+        <button class="dropdown-btn">جميع الطلاب
+            <i class="fa fa-caret-down"></i>
+        </button>
+    <div class="dropdown-container">
+        <a href="{{route('all.students.1st')}}"><i class="fas fa-question"></i><span>جميع طلاب الأول الثانوي</span></a>
+        <a href="{{route('all.students.2nd')}}"><i class="fas fa-question"></i><span>جميع طلاب الثاني الثانوي</span></a>
+        <a href="{{route('all.students.3rd')}}"><i class="fas fa-question"></i><span>جميع طلاب الثالث الثانوي</span></a>
+    </div>
 
+        <button class="dropdown-btn"> قائمة الانتظار
+            <i class="fa fa-caret-down"></i>
+        </button>
+    <div class="dropdown-container">
+        <a href="{{url('view/list/1st')}}"><i class="fas fa-question"></i><span>قائمة الانتظار الصف الاول</span></a>
+        <a href="{{url('view/list/2nd')}}"><i class="fas fa-question"></i><span>قائمة الانتظار الصف الثاني</span></a>
+        <a href="{{url('view/list/3rd')}}"><i class="fas fa-question"></i><span>قائمة الانتظار الصف الثالث</span></a>
+    </div>
 
-
-{{--      <a href="about.html"><i class="fas fa-question"></i><span>about</span></a>--}}
-{{--       all/students/1st/year --}}
-       <a href="{{route('all.students.1st')}}"><i class="fas fa-question"></i><span>All Students 1st</span></a>
-       <a href="{{route('all.students.2nd')}}"><i class="fas fa-question"></i><span>All Students 2nd </span></a>
-       <a href="{{route('all.students.3rd')}}"><i class="fas fa-question"></i><span>All Students 3rd</span></a>
-
-      <a href="{{url('view/list/1st')}}"><i class="fas fa-question"></i><span>Waiting list 1st</span></a>
-      <a href="{{url('view/list/2nd')}}"><i class="fas fa-question"></i><span>Waiting list 2nd</span></a>
-      <a href="{{url('view/list/3rd')}}"><i class="fas fa-question"></i><span>Waiting list 3rd</span></a>
-      <a href="courses.html"><i class="fas fa-graduation-cap"></i><span>courses</span></a>
-{{--      <a href="teachers.html"><i class="fas fa-chalkboard-user"></i><span>teachers</span></a>--}}
-{{--      <a href="contact.html"><i class="fas fa-headset"></i><span>contact us</span></a>--}}
-   </nav>
+    </nav>
 
 </div>
 
-
 <section class="courses">
 
-   <h1 class="heading">our courses</h1>
+   <h1 class="heading">Admin Dashboard</h1>
 
    <div class="box-container">
 
       <div class="box">
-         <div class="tutor">
-            <img src="{{asset('images/pic-2.jpg')}}" alt="">
-            <div class="info">
-               <h3>john deo</h3>
-               <span>21-10-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <img src="{{asset('images/thumb-1.png')}}" alt="">
-            <span>10 videos</span>
-         </div>
          <h3 class="title">complete HTML tutorial</h3>
          <a href="playlist.html" class="inline-btn">view playlist</a>
       </div>
 
       <div class="box">
-         <div class="tutor">
-            <img src="{{asset('images/pic-3.jpg')}}" alt="">
-            <div class="info">
-               <h3>john deo</h3>
-               <span>21-10-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <img src="{{asset('images/thumb-2.png')}}" alt="">
-            <span>10 videos</span>
-         </div>
          <h3 class="title">complete CSS tutorial</h3>
          <a href="playlist.html" class="inline-btn">view playlist</a>
       </div>
 
       <div class="box">
-         <div class="tutor">
-            <img src="{{asset('images/pic-4.jpg')}}" alt="">
-            <div class="info">
-               <h3>john deo</h3>
-               <span>21-10-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <img src="{{asset('images/thumb-3.png')}}" alt="">
-            <span>10 videos</span>
-         </div>
          <h3 class="title">complete JS tutorial</h3>
          <a href="playlist.html" class="inline-btn">view playlist</a>
       </div>
 
       <div class="box">
-         <div class="tutor">
-            <img src="{{asset('images/pic-5.jpg')}}" alt="">
-            <div class="info">
-               <h3>john deo</h3>
-               <span>21-10-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <img src="{{asset('images/thumb-4.png')}}" alt="">
-            <span>10 videos</span>
-         </div>
          <h3 class="title">complete Boostrap tutorial</h3>
          <a href="playlist.html" class="inline-btn">view playlist</a>
       </div>
 
       <div class="box">
-         <div class="tutor">
-            <img src="{{asset('images/pic-6.jpg')}}" alt="">
-            <div class="info">
-               <h3>john deo</h3>
-               <span>21-10-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <img src="{{asset('images/thumb-5.png')}}" alt="">
-            <span>10 videos</span>
-         </div>
-         <h3 class="title">complete JQuery tutorial</h3>
+         <h3 class="title">complete Boostrap tutorial</h3>
          <a href="playlist.html" class="inline-btn">view playlist</a>
       </div>
 
       <div class="box">
-         <div class="tutor">
-            <img src="{{asset('images/pic-7.jpg')}}" alt="">
-            <div class="info">
-               <h3>john deo</h3>
-               <span>21-10-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <img src="{{asset('images/thumb-6.png')}}" alt="">
-            <span>10 videos</span>
-         </div>
-         <h3 class="title">complete SASS tutorial</h3>
+         <h3 class="title">complete Boostrap tutorial</h3>
          <a href="playlist.html" class="inline-btn">view playlist</a>
       </div>
 
-   </div>
+      <div class="box">
+         <h3 class="title">complete Boostrap tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
 
-   <div class="more-btn">
-      <a href="courses.html" class="inline-option-btn">view all courses</a>
-   </div>
+      <div class="box">
+         <h3 class="title">complete Boostrap tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
 
+      <div class="box">
+         <h3 class="title">complete Boostrap tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+      
 </section>
 
+
+
+
+<!-- Start Footer -->
+<footer class="footer">
+    <div class="container">
+        <div class="box">
+            <a href="home.html" class="logo"><img src="{{asset('images/logo.png')}}"></a>
+            <ul class="social">
+                <li>
+                    <a href="#" class="facebook">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="twitter">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="youtube">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </li>
+            </ul>
+            <p class="text">
+                منصة علاء الدين لشرح منهج الفزياء للثانوية العامة
+            </p>
+        </div>
+        <div class="box">
+            <ul class="links">
+                <li><a href="home.html">الرئيسية</a></li>
+                <li><a href="about.html">من نحن</a></li>
+                <li><a href="contact.html">تواصل معنا</a></li>
+                <li><a href="courses.html">الكورسات</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <div class="line">
+                <i class="fas fa-map-marker-alt fa-fw"></i>
+                <div class="info">مصر </div>
+            </div>
+            <div class="line">
+                <i class="far fa-clock fa-fw"></i>
+                <div class="info">24/7</div>
+            </div>
+            <div class="line">
+                <i class="fas fa-phone-volume fa-fw"></i>
+                <div class="info">
+                    <span>+201149596478</span>
+                </div>
+            </div>
+        </div>
+        <div class="box footer-gallery">
+            <img src="{{asset('images/thumb-9.png')}}" alt="" />
+            <img src="{{asset('images/thumb-8.png')}}" alt="" />
+            <img src="{{asset('images/thumb-7.png')}}" alt="" />
+            <img src="{{asset('images/thumb-6.png')}}" alt="" />
+            <img src="{{asset('images/thumb-5.png')}}" alt="" />
+            <img src="{{asset('images/thumb-4.png')}}" alt="" />
+        </div>
+    </div>
+    <p class="copyright">Developed By <a href="#" class="fas fa-heart"></a> By Anas , Ahmed</p>
+    &copy; copyright @ 2022 | all rights reserved!
+</footer>
+
+<!-- scroll top button  -->
+<a href="#" class="top">
+    <img src="{{asset('images/scroll-top-img.png')}}">
+</a>
+<!-- End Footer -->
 <script>
-    let body = document.body;
-    let sideBar = document.querySelector('.side-bar');
-    document.querySelector('#menu-btn').onclick = () =>{
-        sideBar.classList.toggle('active');
-        body.classList.toggle('active');
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
+let sideBar = document.querySelector('.side-bar');
+document.querySelector('#menu-btn').onclick = () =>{
+   sideBar.classList.toggle('active');
+   body.classList.toggle('active');
+}
+
+document.querySelector('#close-btn').onclick = () =>{
+   sideBar.classList.remove('active');
+   body.classList.remove('active');
+}
+
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+    search.classList.remove('active');
+}
+window.onscroll = () =>{
+    profile.classList.remove('active');
+   if(window.innerWidth < 1200){
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
+}
+
+
+// scroll to top
+let up = document.querySelector(".top");
+
+window.onscroll = function () {
+    console.log(window.scrollY);
+    if (window.scrollY >= 300) {
+        up.style.display = "block";
+    } else {
+        up.style.display = "none";
     }
+   //  this.scrollY >= 1000 ? span.classList.add("show") : span.classList.remove("show");
+};
+
+up.onclick = function () {
+    window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+    });
+};
+
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
 </script>
 
+
 @endsection
+
+
