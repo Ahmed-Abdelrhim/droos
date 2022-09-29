7ae511e9efe872f3fc477bf803f90127be162ac3
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-09-29 19:06:11 +0200

    disable right click

--- resources/views/student/enrolled/third/lecture.blade.php
+++ resources/views/student/enrolled/third/lecture.blade.php
@@ -119,54 +119,54 @@
 
 
 
-{{--@section('script')--}}
-{{--    <script language=JavaScript>--}}
-{{--        var message="This function has been disabled!";--}}
+@section('script')
+    <script language=JavaScript>
+        var message="This function has been disabled!";
 
-{{--        ///////////////////////////////////--}}
+        ///////////////////////////////////
 
-{{--        function clickIE4(){--}}
+        function clickIE4(){
 
-{{--            if (event.button==2){--}}
+            if (event.button==2){
 
-{{--                alert(message);--}}
+                alert(message);
 
-{{--                return false;--}}
+                return false;
 
-{{--            }--}}
+            }
 
-{{--        }--}}
+        }
 
-{{--        function clickNS4(e){--}}
+        function clickNS4(e){
 
-{{--            if (document.layers||document.getElementById&&!document.all){--}}
+            if (document.layers||document.getElementById&&!document.all){
 
-{{--                if (e.which==2||e.which==3){--}}
+                if (e.which==2||e.which==3){
 
-{{--                    alert(message);--}}
+                    alert(message);
 
-{{--                    return false;--}}
+                    return false;
 
-{{--                }--}}
+                }
 
-{{--            }--}}
+            }
 
-{{--        }--}}
+        }
 
-{{--        if (document.layers){--}}
+        if (document.layers){
 
-{{--            document.captureEvents(Event.MOUSEDOWN);--}}
+            document.captureEvents(Event.MOUSEDOWN);
 
-{{--            document.onmousedown=clickNS4;--}}
+            document.onmousedown=clickNS4;
 
-{{--        }--}}
+        }
 
-{{--        else if (document.all&&!document.getElementById){--}}
+        else if (document.all&&!document.getElementById){
 
-{{--            document.onmousedown=clickIE4;--}}
+            document.onmousedown=clickIE4;
 
-{{--        }--}}
-{{--        document.oncontextmenu=new Function("alert(message);return false")--}}
-{{--    </script>--}}
-{{--@endsection--}}
+        }
+        document.oncontextmenu=new Function("alert(message);return false")
+    </script>
+@endsection
 
