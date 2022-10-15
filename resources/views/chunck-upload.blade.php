100e78205fcc4bbba6a1ed9773c9f6b4cd1c9b2c
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2022-10-15 02:49:23 +0200

    update

--- resources/views/chunck-upload.blade.php
+++ resources/views/chunck-upload.blade.php
@@ -17,79 +17,81 @@
         display: none;
     } */
 
-        .progress{
+    .progress {
         position: relative;
         width: 100%;
         /* background-color: #c9cfc9; */
     }
 
-    .bar{
+    .bar {
         background-color: #237eb7;
         width: 0%;
         height: 20px;
     }
 
-.copy {
-    color: #03a9f4;
-    border-color: #03a9f4;
-    background: linear-gradient(to right, #03a9f4 50%, white 50%);
-    background-size: 200% 100%;
-    background-position: right bottom;
-}
+    .copy {
+        color: #03a9f4;
+        border-color: #03a9f4;
+        background: linear-gradient(to right, #03a9f4 50%, white 50%);
+        background-size: 200% 100%;
+        background-position: right bottom;
+    }
 
-.copy {
-    background-position: left bottom;
-    color: white;
-}
+    .copy {
+        background-position: left bottom;
+        color: white;
+    }
 
 </style>
 
 <body>
 
 
-    <section class="form-container">
+<section class="form-container" style="width: 50%;">
 
     <div class="form">
-    <div>
-        <h1 id="video_name"></h1>
-        <button class="copybtn copy">Copy</button>
-    </div>
+        <div>
+            <h1 id="video_name"></h1>
+            <button class="copybtn copy">Copy</button>
+        </div>
 
-<div class="container pt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header text-center">
-                    <h3>Add New video </h3>
-                </div>
-
-                <div class="progress mt-3" style="height: 25px">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated bar" role="progressbar"
-                             aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%; height: 100%">
-                            0%
+        <div class="container pt-4">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <h3>Add New video </h3>
                         </div>
-                    </div>
 
-                <p>Choose a video to add<span>*</span></p>
-                <div class="card-body">
-                    <div id="upload-container" class="text-center">
-                        <button id="browseFile" class="btn btn-primary">Brows File</button>
-                    </div>
+                        <div class="progress mt-3" style="height: 25px">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bar" role="progressbar"
+                                 aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"
+                                 style="width: 0%; height: 100%">
 
-                    <p>Upload video <span>*</span></p>
-                    <button class="btn btn-primary" id="submit">upload</button>
+                            </div>
+                        </div>
+
+                        {{-- <p>Choose a video to add<span>*</span></p>--}}
+                        <div class="card-body">
+                            <div id="upload-container" class="text-center">
+                                {{--<button id="browseFile" class="btn btn-primary">Brows File</button>--}}
+                                <input type="file" id="browseFile">
+                            </div>
+
+                            <p>Upload video <span>*</span></p>
+                            <button class="btn btn-primary" id="submit" style="width: 50%; margin-left: 25%;">upload
+                            </button>
+                        </div>
+
+                    </div>
                 </div>
-
             </div>
         </div>
-    </div>
-</div>
 
 
     </div>
 
-    </section>
-
+</section>
 
 
 <!-- jQuery -->
@@ -139,7 +141,7 @@
         updateProgress(Math.floor(file.progress() * 100));
     });
 
-    resumable.on('fileSuccess', function ( file,response) { // trigger when file upload complete
+    resumable.on('fileSuccess', function (file, response) { // trigger when file upload complete
         {{--$.ajax({--}}
         {{--    url: "{{ route('post.post')}}",--}}
         {{--    method: 'POST',--}}
@@ -196,8 +198,6 @@
     }
 
 
-
-
     // $('#submit').on('click',function (){
     //     console.log('hey');
     //     $('#academic_year').on('change', function () {
@@ -220,15 +220,15 @@
     //     });
     // });
 
-var copyTextareaBtn = document.querySelector('.copybtn');
+    var copyTextareaBtn = document.querySelector('.copybtn');
 
-copyTextareaBtn.addEventListener('click', function(event) {
-  var copy_text = document.getElementsByTagName("h1")[0];
-  var range = document.createRange();
-  range.selectNode(copy_text);
-  window.getSelection().addRange(range);
+    copyTextareaBtn.addEventListener('click', function (event) {
+        var copy_text = document.getElementsByTagName("h1")[0];
+        var range = document.createRange();
+        range.selectNode(copy_text);
+        window.getSelection().addRange(range);
 
-});
+    });
 
 </script>
 </body>
